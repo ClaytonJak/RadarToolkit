7552bb61a5054dff56f7c0f1b1aee1539526107e
Clayton Jaksha <claytonjak@gmail.com>
2025-07-26 10:12:56 -0700

    Added wgn

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import numpy as np
 import matplotlib as mpl
+import wgn
+
+# generate an array for signal
+size_test_sig = 100
+test_sig = np.zeros(size_test_sig)
+test_sig[int(size_test_sig/2)] = 1
+
+Y = wgn.awgn(test_sig,1)
+print(Y)
+
+
---
+++ wgn.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def awgn(X,SNR):
+    Y = X
+    return Y
