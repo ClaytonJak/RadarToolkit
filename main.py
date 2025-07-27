58f0b323996375a69ba109060e9e3bf0d3f8681c
Clayton Jaksha <claytonjak@gmail.com>
2025-07-26 21:04:21 -0700

    pulsed waveform

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ sample_rate = 5e9 #samples/sec
 
 # generate an array for signal
 X = toolkit.CW_waveform(1,5e-6,sample_rate,1e9)
-spec = np.fft.fft(X)
+pwr_X = np.abs(X)
+#spec = np.fft.fft(X)
 
 # plot the clean and noisy signals on top of each other
-#s = range(0,len(noisy_sig))
+s = range(0,len(pwr_X))
 fig, axs = plt.subplots()
 axs.set_title("Signal")
-axs.plot(spec,'b')
+axs.plot(pwr_X,'b')
 #axs.plot(s, noisy_sig,'r')
 axs.set_xlabel("Sample")
 axs.set_ylabel("Amplitude")
--- toolkit.py
+++ toolkit.py
@@ -1,3 +1,4 @@
+from pickle import FALSE, TRUE
 import numpy as np
 
 # Note: do not remove awgn() from the same file as signal_power()
@@ -23,4 +24,29 @@ def CW_waveform(amp,t_len,sample_rate,freq):
     for n in range(0,len(X)):
         t = n/sample_rate
         X[n] = amp*np.exp(1j*w_t*t)
+    return X
+
+def pulsed_waveform(amp,t_len,sample_rate,freq,pulse_width,PRF):
+    n_samples = int(t_len*sample_rate)
+    X = np.zeros(n_samples, dtype = 'complex_')
+    w_t = 2*np.pi*freq
+    PRI = 1/PRF
+    in_pulse_flag = FALSE
+    PRI_multiple = 0
+    for n in range(0,len(X)):
+        t = n/sample_rate
+        if in_pulse_flag == FALSE:
+            if t > (PRI*PRI_multiple):
+                in_pulse_flag = TRUE
+                PRI_multiple += 1
+                finish_pulse = t + pulse_width
+        if in_pulse_flag == TRUE:
+            if t < finish_pulse:
+                ampl = amp
+            else:
+                in_pulse_flag = FALSE
+                ampl = 0
+        else:
+            ampl = 0
+        X[n] = ampl*np.exp(1j*w_t*t)
     return X
