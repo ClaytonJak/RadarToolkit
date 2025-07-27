90a2fed2e96651df37f4664dfa5e9c415f8045ce
Clayton Jaksha <claytonjak@gmail.com>
2025-07-27 11:58:47 -0700

    added comments

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import toolkit
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+import toolkit as tk
 
 # seed random numbers numpy
 np.random.seed(0)
 
 # initialize matplotlib parameters
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.figsize"] = [7.50, 5.00]
 plt.rcParams["figure.autolayout"] = True
 
 # initialize sample rate
-sample_rate = 2e9 #samples/sec
+sample_rate = 3e9 #samples/sec
+
+# initialize my target state
+target = {
+    "range" : 1000, #meters
+    "rate" : -100, #m/s, negative closing, positive opening
+    "RCS" : 10, # dBsm, Swerling 0
+    }
 
 # generate an array for signal
-X = toolkit.pulsed_waveform(1,5e-3,sample_rate,1e9,100e-6,1e3)
-pwr_X = np.abs(X)
-#spec = np.fft.fft(X)
+X,M = tk.chirped_waveform_single(1,sample_rate,1e9,10e6,100e-6,1e3)
+pwr_X = (np.abs(M))
+spec = 10*np.log10(np.abs(np.fft.fft(M)))
 
 # plot the clean and noisy signals on top of each other
-s = range(0,len(pwr_X))
-fig, axs = plt.subplots()
-axs.set_title("Signal")
-#axs.plot(pwr_X,'b')
-axs.plot(spec,'r')
-axs.set_xlabel("Sample")
-axs.set_ylabel("Amplitude")
+s = np.array(range(0,len(pwr_X)))
+s = s/sample_rate
+fig, axs = plt.subplots(2)
+fig.suptitle('Signal Time and Freq Domain Analysis')
+axs[0].set_title("Signal Amplitude")
+axs[0].plot(s,pwr_X,'b')
+axs[0].set_xlabel("Time [s]")
+axs[0].set_ylabel("Amplitude")
+axs[0].xaxis.set_minor_locator(AutoMinorLocator())
+axs[0].yaxis.set_minor_locator(AutoMinorLocator())
+axs[0].grid(visible=True, which='both',axis='both',color='0.8', linestyle='-', linewidth=1)
+del s
+# plot the clean and noisy signals on top of each other
+s = np.array(range(0,len(spec)))
+s = s*sample_rate/len(spec)
+axs[1].set_title("FFT Output")
+axs[1].plot(s,spec,'r')
+axs[1].set_xlabel("Sampled Frequency")
+axs[1].set_ylim([-20,80])
+axs[1].set_ylabel("Power [dB]")
+axs[1].grid(visible=True, which='both',axis='both',color='0.8', linestyle='-', linewidth=1)
+axs[1].xaxis.set_minor_locator(AutoMinorLocator())
+axs[1].yaxis.set_minor_locator(AutoMinorLocator())
 plt.show()
 del s
-
--- toolkit.py
+++ toolkit.py
@@ -1,8 +1,16 @@
 from pickle import FALSE, TRUE
 import numpy as np
+import scipy.constants
+
+# Constants
+c = scipy.constants.c; #m/s, speed of light
 
 # Note: do not remove awgn() from the same file as signal_power()
 def awgn(X,SNR_dB):
+    # description - this adds white gaussian noise to a numpy array signal
+    # input X is the input numpy array 1xN
+    # input SNR_dB is the desired SNR of the power of X to the power (covariance) of WGN in the output matrix
+    # output Y is numpy array 1xN with additave white gaussian noise
     Y = X.copy()
     SNR_lin = np.power(10,SNR_dB/10)
     noise_cov = signal_power(X)/SNR_lin
@@ -13,11 +21,20 @@ def awgn(X,SNR_dB):
     return Y
 
 def signal_power(X):
+    # description - this calculates the power of a numpy array signal
+    # input X is the 1xN numpy array signal
+    # output pwr is a float average output power of the signal
     L = len(X)
     pwr = np.sum(np.power(X,2))/L
     return pwr
 
 def CW_waveform(amp,t_len,sample_rate,freq):
+    # description - this generates a continuous wave (CW) waveform in a 1xN numpy array
+    # input amp is a float/int for desired CW amplitude
+    # input t_len is a float/int for the desired time length (in seconds) of the CW waveform
+    # input sample_rate is the sample rate (in samples/sec) of the sampled CW waveform
+    # input freq is the frequency of the CW waveform (in Hz)
+    # output X is the 1xN numpy array containing the CW waveform
     n_samples = int(t_len*sample_rate)
     X = np.zeros(n_samples, dtype = 'complex_')
     w_t = 2*np.pi*freq
@@ -26,7 +43,40 @@ def CW_waveform(amp,t_len,sample_rate,freq):
         X[n] = amp*np.exp(1j*w_t*t)
     return X
 
-def pulsed_waveform(amp,t_len,sample_rate,freq,pulse_width,PRF):
+def pulsed_waveform_single(amp,sample_rate,freq,pulse_width,PRF):
+    # description - this generates a single pulsed waveform in a 1xN numpy array
+    # input amp is a float/int for the desired CW amplitude
+    # input sample_rate is the sample rate (in samples/sec) of the sampled pulsed waveform
+    # input freq is the center frequency of the pulsed waveform (in Hz)
+    # input pulse_width is the pulse width of the pulsed waveform (in seconds)
+    # input PRF is the pulse repitition frequency (PRF) of the pulsed waveform
+    # output X is a 1xN numpy array containing one single PRI of the pulsed waveform
+    # output M is a 1x(pulse width) array containing only the pulsed portion of the pulsed waveform (for matched filter purposes)
+    PRI = 1/PRF
+    n_samples = int(PRI*sample_rate)
+    X = np.zeros(n_samples, dtype = 'complex_')
+    w_t = 2*np.pi*freq
+    marker = 0
+    for n in range(0,len(X)):
+        t = n/sample_rate
+        if t < pulse_width:
+            ampl = amp
+            marker = n
+        else:
+            ampl = 0
+        X[n] = ampl*np.exp(1j*w_t*t)
+    M = X[:marker]
+    return X,M
+
+def pulsed_waveform_multi(amp,t_len,sample_rate,freq,pulse_width,PRF):
+    # description - this generates a specified time sample of a pulsed waveform in a 1xN numpy array
+    # input amp is a float/int for the desired amplitude
+    # input t_len is a float/int for the desired time lenth of the output waveform (in seconds)
+    # input sample_rate is the sample rate (in samples/sec) of the sampled pulsed waveform
+    # input freq is the center frequency of the pulsed waveform (in Hz)
+    # input pulse_width is the pulse width of the pulsed waveform (in seconds)
+    # input PRF is the pulse repitition frequency (PRF) of the pulsed waveform
+    # output X is a 1xN numpy array containing one single PRI of the pulsed waveform
     n_samples = int(t_len*sample_rate)
     X = np.zeros(n_samples, dtype = 'complex_')
     w_t = 2*np.pi*freq
@@ -50,3 +100,54 @@ def pulsed_waveform(amp,t_len,sample_rate,freq,pulse_width,PRF):
             ampl = 0
         X[n] = ampl*np.exp(1j*w_t*t)
     return X
+
+def chirped_waveform_single(amp,sample_rate,freq,BW,pulse_width,PRF):
+    # description - this generates a single chirped waveform in a 1xN numpy array
+    # input amp is a float/int for the desired amplitude
+    # input sample_rate is the sample rate (in samples/sec) of the sampled chirped waveform
+    # input freq is the center frequency of the chirped waveform (in Hz)
+    # input BW is the 3dB bandwidth of the chirped waveform (in Hz)
+    # input pulse_width is the pulse width of the chirped waveform (in seconds)
+    # input PRF is the pulse repitition frequency (PRF) of the chirped waveform
+    # output X is a 1xN numpy array containing one single PRI of the chirped waveform
+    # output M is a 1x(pulse width) array containing only the pulsed portion of the chirped waveform (for matched filter purposes)
+    PRI = 1/PRF
+    n_samples = int(PRI*sample_rate)
+    X = np.zeros(n_samples, dtype = 'complex_')
+    w_t = 2*np.pi*freq
+    w_t_lower = 2*np.pi*(freq-(BW/2))
+    marker = 0
+    for n in range(0,len(X)):
+        t = n/sample_rate
+        if t < pulse_width:
+            ampl = amp
+            marker = n
+        else:
+            ampl = 0
+            W_t = w_t
+        X[n] = ampl*np.exp(1j*((w_t_lower*t)+(np.pi*(BW/pulse_width)*(np.power(t,2)))))
+    M = X[:marker]
+    return X,M
+
+def return_pulse(truth_range, truth_range_rate,truth_RCS,truth_addl_loss,X,SNR,sample_rate):
+    # description - takes a TX waveform, reduces its power by free space path loss, RCS return, 
+    #               and additional desired losses, then applies WGN at a desired SNR to the 
+    #               return signal. Doppler shifts the return according to the target rate.
+    #               Zero pads the beginning of the waveform to express the range.
+    #               Returns a RX waveform with the signal buried in the noise.
+    # input truth_range is the truth range to the target in meters
+    # input truth_range_rate is the truth range rate to the target in m/s. Closing neg, opening pos.
+    # input truth_RCS is the truth Swerling 0 RCS (in dBsm)
+    # input truth_addl_loss is a place to account for different losses (in dB) not in free-space
+    #               path loss (e.g. TX/RX chain, atmospherics, etc). Positive values are losses.
+    # input X is the transmitted waveform
+    # input SNR is the desired SNR (in dB) of the RX waveform (after losses) to WGN
+    # input sample_rate is the sample rate of the TX waveform (will also be applied to RX waveform)
+    # output Y is the RX waveform (will likely look like a signal buried in noise)
+    Y = X.copy()
+
+    t_delay = 2*truth_range/c
+    n_delay = t_delay*sample_rate
+    delay_array = np.zeros(int(n_delay))
+
+    return Y
