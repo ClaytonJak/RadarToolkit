a89a2deb8eccb5c16ab95c627912a27d824c778a
Clayton Jaksha <claytonjak@gmail.com>
2025-07-26 21:28:08 -0700

    push

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
 # initialize sample rate
-sample_rate = 5e9 #samples/sec
+sample_rate = 2e9 #samples/sec
 
 # generate an array for signal
 X = toolkit.pulsed_waveform(1,5e-3,sample_rate,1e9,100e-6,1e3)
@@ -21,8 +21,8 @@ pwr_X = np.abs(X)
 s = range(0,len(pwr_X))
 fig, axs = plt.subplots()
 axs.set_title("Signal")
-axs.plot(pwr_X,'b')
-#axs.plot(s, noisy_sig,'r')
+#axs.plot(pwr_X,'b')
+axs.plot(spec,'r')
 axs.set_xlabel("Sample")
 axs.set_ylabel("Amplitude")
 plt.show()
